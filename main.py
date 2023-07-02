2cb70996e84d87207e0b1d6dd687753f7b085ff6
zonzozon <87434882+zonzozon@users.noreply.github.com>
2023-07-02 15:50:54 +0200

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import random
 
 def guess_number():
-    target_number = random.randint(1, 10)
-        attempts = 0
+    min_number = int(input("Enter the minimum number: "))
+    max_number = int(input("Enter the maximum number: "))
+    attempts = int(input("Enter the number of attempts: "))
 
-            while True:
-                    guess = int(input("Guess a number between 1 and 10: "))
-                            attempts += 1
+    target_number = random.randint(min_number, max_number)
 
-                                    if guess == target_number:
-                                                print("You won!")
-                                                            break
-                                                                    else:
-                                                                                print("You lost! Try again.")
+    while attempts > 0:
+        guess = int(input(f"Guess a number between {min_number} and {max_number}: "))
 
-                                                                                        if attempts >= 3:
-                                                                                                    print("You have reached the maximum number of attempts.")
-                                                                                                                print("The target number was", target_number)
-                                                                                                                            break
+        if guess == target_number:
+            print("You won!")
+            return
 
-                                                                                                                            guess_number()
+        print("You lost! Try again.")
+        attempts -= 1
+
+    print("You have reached the maximum number of attempts.")
+    print("The target number was", target_number)
+
+guess_number()
