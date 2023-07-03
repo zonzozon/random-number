86e605190f30d7320f4da33a8c9187c8e98a3412
zonzozon <87434882+zonzozon@users.noreply.github.com>
2023-07-03 13:48:39 +0200

    Fixed a bug

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,6 +1,6 @@
 import random
 
-def play_guess_the_number():
+def play_guess_user_number():
     def guess_number():
         min_number = int(input("Enter the minimum number: "))
         max_number = int(input("Enter the maximum number: "))
@@ -13,12 +13,22 @@ def play_guess_the_number():
 
             if guess == target_number:
                 print("You won!")
-                return
+                return True
 
             print("You lost! Try again.")
             attempts -= 1
 
         print("You have reached the maximum number of attempts.")
         print("The target number was", target_number)
-    
-    guess_number()
+        return False
+
+    play_again = input("Do you want to continue with the game? [1] Yes, [2] Change game: ")
+    while play_again not in ['1', '2']:
+        print("Invalid input! Please enter [1] to continue with the game or [2] to change the game.")
+        play_again = input("Do you want to continue with the game? [1] Yes, [2] Change game: ")
+
+    if play_again == '1':
+        if not guess_number():
+            play_guess_user_number()
+
+play_guess_user_number()
